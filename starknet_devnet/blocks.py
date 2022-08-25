ff71c467520e23dd73bfc1129b51af32c46de484
internos <amajidsinar@gmail.com>
2022-08-25 12:59:01 +0100

    refactor: empty block creation

--- starknet_devnet/blocks.py
+++ starknet_devnet/blocks.py
@@ -90,7 +90,7 @@ class DevnetBlocks():
 
     async def generate(
         self, transaction: DevnetTransaction, state: StarknetState,
-        state_root: bytes, state_update = None, is_empty = False
+        state_root: bytes, state_update = None, is_empty_block = False
     ) -> StarknetBlock:
         """
         Generates a block and stores it to blocks and hash2block. The block contains just the passed transaction.
@@ -99,25 +99,24 @@ class DevnetBlocks():
         """
         block_number = self.get_number_of_blocks()
         timestamp = state.state.block_info.block_timestamp
-        signature = transaction.get_signature()
-
+        
         if block_number == 0:
             parent_block_hash = 0
         else:
             last_block = self.__get_last_block()
             parent_block_hash = last_block.block_hash
 
-        if self.lite:
-            block_hash = block_number
-
-        if is_empty:
+        if is_empty_block:
             transaction_receipts = ()
             transactions = []
             block_hash = block_number
         else:
-            transaction_receipts = (transaction.get_execution(),)
-            transactions = [transaction.internal_tx]
-            block_hash = await calculate_block_hash(
+           
+            if self.lite:
+                block_hash = block_number
+            else:
+                signature = transaction.get_signature()
+                block_hash =  await calculate_block_hash(
                 general_config=state.general_config,
                 parent_hash=parent_block_hash,
                 block_number=block_number,
@@ -127,8 +126,11 @@ class DevnetBlocks():
                 tx_signatures=[signature],
                 event_hashes=[],
                 sequencer_address=state.general_config.sequencer_address
-            )
-            
+                )
+
+            transaction_receipts = (transaction.get_execution(),)
+            transactions=[transaction.internal_tx]
+                
         block = StarknetBlock.create(
             block_hash=block_hash,
             block_number=block_number,
